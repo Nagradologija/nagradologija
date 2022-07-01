d29448ff022098132537fe272aa44042b0c6bd94
ikusteu <ivan.kust57@gmail.com>
2022-07-01 21:41:38 +0200

    Initial commit

---
+++ apps/web/src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import '../../src/main.css';
+</script>
+
+<svelte:head>
+	<title>Nagradologija</title>
+</svelte:head>
+
+<slot />
---
+++ apps/web/src/routes/index.svelte
@@ -0,0 +1,2 @@
+<h1>NAGRADOLOGIJA !!!</h1>
+<p>Luxurence....</p>
